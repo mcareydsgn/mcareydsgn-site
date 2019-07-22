e614a2494256f280ca37ae8cd60f35bd34ee3839
Matthew Carey <matthewcarey@Matthews-Mac-mini.local>
2019-07-21 20:08:38 -0700

    Added fonts, and images for header

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -3,12 +3,12 @@
     <h1>{{ msg }}</h1>
     <p>
       For a guide and recipes on how to configure / customize this project,<br />
-      check out the
+      check out theaaa
       <a href="https://cli.vuejs.org" target="_blank" rel="noopener"
         >vue-cli documentation</a
       >.
     </p>
-    <h3>Installed CLI Plugins</h3>
+    <h3>Installed CLI Pluginsa</h3>
     <ul>
       <li>
         <a
---
+++ src/components/SplashHeader.vue
@@ -0,0 +1,51 @@
+<template>
+  <header>
+    <div class="pictureContainer">
+      <img id="picture" src="../assets/mcarey-picture.png" alt />
+    </div>
+    <div class="nameContainer">
+      <h1>MATTHEW CAREY</h1>
+      <h2>
+        <span>A CREATIVE</span>
+        <span>DESIGNER</span>
+      </h2>
+    </div>
+    <div class="ampersandContainer">
+      <img src="../assets/ampersand.svg" alt="and" />
+    </div>
+    <div class="devContainer">
+      <span class="smallTitle">FRONT-END</span>
+      <h2>
+        <span>WEB</span>
+        <span>DEVELOPER</span>
+      </h2>
+    </div>
+    <div class="backgroundContainer">
+      <div id="rectSolid"></div>
+    </div>
+  </header>
+</template>
+
+<script>
+export default {
+  name: "SplashHeader"
+};
+</script>
+
+<style scoped lang="scss">
+header {
+  position: relative;
+
+  & div[class*="Container"] {
+    position: absolute;
+  }
+}
+
+h1 {
+  font-family: $displayFont;
+  color: $brightBlue;
+}
+img {
+  width: 100%;
+}
+</style>
